6488c41408bab2976f741871d56386381baf917f
= <suman.jha108155@gmail.com>
2024-01-07 11:10:27 +0530

    feat: make the changes to entry point of the application
    The entry point has been updated to have container class
    
    Closes none

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import "./index.css";
 function App() {
   return (
     <>
-      <div className="p-5 md:p-10">
+      <div className="container p-10">
         <Navbar />
         <Banner />
       </div>
