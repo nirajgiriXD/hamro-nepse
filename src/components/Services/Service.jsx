e71183b499752664cacf6445e11d00992a953f05
Sandesh Ghimire <62834029+Sandesh-Ghimire@users.noreply.github.com>
2024-01-13 14:33:27 +0530

    Update src/components/Services/Service.jsx
    Co-authored-by: Niraj Giri <51747980+nirajgiriXD@users.noreply.github.com>

--- src/components/Services/Service.jsx
+++ src/components/Services/Service.jsx
@@ -29,7 +29,7 @@ const Service = () => {
         <p className="leading-relaxed text-base">Lorem Ipsum</p>
       </div>
     </a>
-    <a href="/neptune" className="xl:w-1/3 md:w-1/2 p-6 cursor-pointer">
+    <a href="#" className="xl:w-1/3 md:w-1/2 p-6 cursor-pointer">
       <div className="border border-gray-300 p-6 rounded-lg">
         <div className="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
           <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-6 h-6" viewBox="0 0 24 24">
