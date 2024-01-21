85b6014459fb45e242f36768f89411df7a9c86ea
nirajgiriXD <nirajgiri9860@googlemail.com>
2024-01-21 22:50:27 +0545

    Implement route

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,9 @@
 /**
+ * External dependencies.
+ */
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+/**
  * Internal dependencies.
  */
 import "./index.css";
@@ -7,16 +12,24 @@ import { Navbar, Services, Banner, Footer } from "./components";
 
 function App() {
   return (
-    <div className="dark:bg-gray-900">
-      <div className="container mx-auto">
-        <div className="w-100 max-w-screen-xl mx-auto px-5">
-          <Navbar logo={logo} />
-          <Banner />
-          <Services />
-          <Footer logo={logo} />
+    <Router>
+      <div className="dark:bg-gray-900">
+        <div className="container mx-auto">
+          <div className="w-100 max-w-screen-xl mx-auto px-5">
+            {/* Navbar */}
+            <Navbar logo={logo} />
+
+            <Routes>
+              <Route path="/" element={<Banner />} />
+              <Route path="/services" element={<Services />} />
+            </Routes>
+
+            {/* Footer */}
+            <Footer logo={logo} />
+          </div>
         </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
