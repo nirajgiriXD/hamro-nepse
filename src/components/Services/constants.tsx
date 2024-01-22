32505430cb8d539003b3e7f2d350451ff850f173
nirajgiriXD <nirajgiri9860@googlemail.com>
2024-01-22 10:00:37 +0545

    Keep service item in seperate file

---
+++ src/components/Services/constants.tsx
@@ -0,0 +1,151 @@
+const ServiceItems = [
+  {
+    icon: (
+      <svg
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        className="w-6 h-6"
+        viewBox="0 0 24 24"
+      >
+        <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
+      </svg>
+    ),
+    title: "IPO Checker",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
+  },
+  {
+    icon: (
+      <svg
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        className="w-6 h-6"
+        viewBox="0 0 24 24"
+      >
+        <circle cx="6" cy="6" r="3"></circle>
+        <circle cx="6" cy="18" r="3"></circle>
+        <path d="M20 4L8.12 15.88M14.47 14.48L20 20M8.12 8.12L12 12"></path>
+      </svg>
+    ),
+    title: "IPO/FPO Updates",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
+  },
+  {
+    icon: (
+      <svg
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        className="w-6 h-6"
+        viewBox="0 0 24 24"
+      >
+        <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
+        <circle cx="12" cy="7" r="4"></circle>
+      </svg>
+    ),
+    title: "Trading Signals",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
+  },
+  {
+    icon: (
+      <svg
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        className="w-6 h-6"
+        viewBox="0 0 24 24"
+      >
+        <path d="M4 15s1-1 4-1 5 2 8 2 4-1 4-1V3s-1 1-4 1-5-2-8-2-4 1-4 1zM4 22v-7"></path>
+      </svg>
+    ),
+    title: "Portfolio Tracker",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
+  },
+  {
+    icon: (
+      <svg
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        className="w-6 h-6"
+        viewBox="0 0 24 24"
+      >
+        <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path>
+      </svg>
+    ),
+    title: "Company Analyzer",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
+  },
+  {
+    icon: (
+      <svg
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        className="w-6 h-6"
+        viewBox="0 0 24 24"
+      >
+        <path d="M21 12.79A9 9 0 1111.21 3 7 7 0 0021 12.79z"></path>
+      </svg>
+    ),
+    title: "Charts",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
+  },
+  {
+    icon: (
+      <svg
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        className="w-6 h-6"
+        viewBox="0 0 24 24"
+      >
+        <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path>
+      </svg>
+    ),
+    title: "Compare Company",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
+  },
+  {
+    icon: (
+      <svg
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        className="w-6 h-6"
+        viewBox="0 0 24 24"
+      >
+        <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path>
+      </svg>
+    ),
+    title: "Share Calculator",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
+  },
+];
+
+export default ServiceItems;
