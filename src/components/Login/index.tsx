892e9d321a8d4f7a90491efefe27fe53784a8d37
sumanjha108155 <suman.jha108155@gmail.com>
2024-01-31 16:18:08 +0530

    feat: add new component for login

---
+++ src/components/Login/index.tsx
@@ -0,0 +1,13 @@
+import LoginItem from "./LoginItem"
+
+const LoginForm = () => {
+    return(
+        <>
+            <div className="grid mx-auto mt-6 lg:mt-8 overflow-hidden text-gray-900 border border-b-1 border-gray-300 border-gray-200 rounded-md lg:grid-cols">
+                <LoginItem/>
+            </div>
+        </>
+    )
+}
+
+export default LoginForm;
