c6afd8bb8bf1ff5ede57cd771a3e677b73bbb134
nirajgiriXD <nirajgiri9860@googlemail.com>
2024-01-31 11:19:51 +0545

    Refcator SimpleTable

--- src/components/SimpleTable/index.tsx
+++ src/components/SimpleTable/index.tsx
@@ -1,38 +1,70 @@
 /**
- * Internal Dependencies.
+ * External Dependencies.
  */
-import Simpletable from "./SimpleTable";
+import {
+  MRT_RowData,
+  MRT_ColumnDef,
+  MaterialReactTable,
+  useMaterialReactTable,
+} from "material-react-table";
+// import { Box, IconButton, Tooltip } from "@mui/material";
+// import FileDownloadIcon from "@mui/icons-material/FileDownload";
+// import { mkConfig, generateCsv, download } from "export-to-csv";
 
-const SimpleTable = () => {
+// const csvConfig = mkConfig({
+//   fieldSeparator: ",",
+//   decimalSeparator: ".",
+//   useKeysAsHeaders: true,
+// });
+
+interface SimpleTableProp {
+  data: MRT_RowData[];
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  columns: MRT_ColumnDef<MRT_RowData, any>[];
+}
+
+const SimpleTable = ({ data, columns }: SimpleTableProp) => {
+  // const handleExportData = () => {
+  //   const csv = generateCsv(csvConfig)(data);
+  //   download(csvConfig)(csv);
+  // };
+
+  const table = useMaterialReactTable({
+    columns,
+    data,
+    enableColumnActions: false,
+    enableColumnFilters: false,
+    enablePagination: false,
+    enableSorting: false,
+    enableGlobalFilter: false,
+    enableBottomToolbar: false,
+    enableFullScreenToggle: false,
+    enableDensityToggle: false,
+    enableHiding: false,
+    enableTopToolbar: false,
+
+    // renderTopToolbarCustomActions: () => (
+    //   <Box
+    //     sx={{
+    //       display: "flex",
+    //       gap: "16px",
+    //       padding: "8px",
+    //       flexWrap: "wrap",
+    //     }}
+    //   >
+    //     <Tooltip arrow title="Download Data">
+    //       <IconButton onClick={handleExportData}>
+    //         <FileDownloadIcon />
+    //       </IconButton>
+    //     </Tooltip>
+    //   </Box>
+    // ),
+  });
+
   return (
-    <>
-      <div className="grid mx-auto text-gray-900 sm:grid-cols-3 md:grid-cols-5 sm:mt-10 lg:grid-cols-9">
-        <button
-          type="button"
-          //onClick={handleTopGainers}
-          className="text-white bg-gradient-to-r from-sky-500 via-sky-600 to-sky-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-sky-300 dark:focus:focus:ring-sky-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
-        >
-          Top Gainers
-        </button>
-        <button
-          type="button"
-          //onClick={handleTopLosers}
-          className="text-white bg-gradient-to-r from-sky-500 via-sky-600 to-sky-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-sky-300 dark:focus:focus:ring-sky-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
-        >
-          Top Losers
-        </button>
-        <button
-          type="button"
-          //onClick={handleTopTurnOvers}
-          className="text-white bg-gradient-to-r from-sky-500 via-sky-600 to-sky-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-sky-300 dark:focus:ring-sky-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
-        >
-          Top Turnover
-        </button>
-      </div>
-      <div className="grid mx-auto mt-6 lg:mt-8 overflow-hidden text-gray-900 border border-b-1 border-gray-300 border-gray-200 rounded-xl lg:grid-cols">
-        <Simpletable />
-      </div>
-    </>
+    <div className="grid mx-auto mt-6 lg:mt-8 overflow-hidden text-gray-900 border border-b-1 border-gray-300 border-gray-200 rounded-md lg:grid-cols">
+      <MaterialReactTable table={table} />
+    </div>
   );
 };
 
