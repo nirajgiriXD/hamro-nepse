b5fb1af45793af72431012249aec1eee327d6cc7
sumanjha108155 <suman.jha108155@gmail.com>
2024-01-31 17:24:41 +0530

    feat: refactor advance table to display market data

--- src/pages/MarketWatchPage/index.tsx
+++ src/pages/MarketWatchPage/index.tsx
@@ -1,5 +1,10 @@
+import MarketWatch from "../../components/AdvanceTable";
+
+const marketwatchendpoint = "https://sam.superintegratedapp.com/wp-json/api/stock-data/?selector=stock&selection=all";
 const MarketWatchPage = () => {
-  return <>Market Watch</>;
+  return <>
+  <MarketWatch fetchURL={marketwatchendpoint}/>
+  </>;
 };
 
 export default MarketWatchPage;
