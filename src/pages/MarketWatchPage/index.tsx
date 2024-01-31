924d748483ee9d8c735fe7ffc49cb2b8547560a5
nirajgiriXD <nirajgiri9860@googlemail.com>
2024-02-01 00:13:10 +0545

    Add date when the stock was last updated

--- src/pages/MarketWatchPage/index.tsx
+++ src/pages/MarketWatchPage/index.tsx
@@ -5,13 +5,13 @@ import { AdvanceTable } from "../../components";
 import useMarketWatchPage from "./useMarketWatchPage";
 
 const MarketWatchPage = () => {
-  const { columns } = useMarketWatchPage();
+  const { date, columns } = useMarketWatchPage();
 
   return (
     <>
       <div className="flex justify-end">
         <span className="text-sm text-gray-900 bg-white dark:bg-gray-800 border border-gray-300 dark:border-gray-600 rounded-md px-5 py-2 mb-2 dark:text-white">
-          2024-01-02
+          {date}
         </span>
       </div>
       <AdvanceTable columns={columns} />
