582454daa7a1240b7f213b34bd7475c36ef684fc
nirajgiriXD <nirajgiri9860@googlemail.com>
2024-02-05 22:21:07 +0545

    Send credentials (cookies) along with request

--- src/store/AppDataProvider.tsx
+++ src/store/AppDataProvider.tsx
@@ -76,7 +76,9 @@ const AppDataProvider = ({ children }: AppDataProviderProp) => {
         "https://sam.superintegratedapp.com/wp-json/api/stock-data/?selector=stock&selection=all";
 
       try {
-        const response = await fetch(url);
+        const response = await fetch(url, {
+          credentials: "include",
+        });
 
         if (!response.ok) {
           throw new Error("Failed to fetch data");
