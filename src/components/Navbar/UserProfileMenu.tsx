3e04d4c4bca2133cdb1410d4204e60adcb52a817
nirajgiriXD <nirajgiri9860@googlemail.com>
2024-02-09 10:18:51 +0545

    Improve UI

--- src/components/Navbar/UserProfileMenu.tsx
+++ src/components/Navbar/UserProfileMenu.tsx
@@ -6,55 +6,48 @@ import { Link } from "react-router-dom";
 /**
  * Internal dependencies.
  */
+import { userProfileDropdownItems } from "../../store/constant";
 import useAppData from "../../store/useAppData";
 import useNavbar from "./useNavbar";
 
-interface UserProfileItem {
-  label: string;
-  href: string;
-}
-
 interface UserProfileDropdownProps {
-  handleDropdownMouseOver: () => void;
-  handleDropdownMouseOut: () => void;
-  userprofileitems: UserProfileItem[];
+  handleOnClick: () => void;
 }
 
-const UserProfileMenu = ({
-  handleDropdownMouseOver,
-  handleDropdownMouseOut,
-  userprofileitems,
-}: UserProfileDropdownProps) => {
+const UserProfileMenu = ({ handleOnClick }: UserProfileDropdownProps) => {
   const { userData } = useAppData();
   const { signOutUser } = useNavbar();
+
+  const handleSignOutClick = () => {
+    signOutUser();
+    handleOnClick();
+    console.log("dsds");
+  };
 
   return (
     <>
       <div
-        onMouseOut={handleDropdownMouseOut}
-        onMouseOver={handleDropdownMouseOver}
-        className="absolute p-2 min-w-40 bg-white dark:bg-gray-900 rounded top-14 drop-shadow-xl z-10 border border-gray-300 dark:border-gray-600 text-gray-700 dark:text-white text-sm"
+        onClick={handleOnClick}
+        className="absolute p-3 min-w-40 bg-white dark:bg-gray-900 rounded-lg top-16 drop-shadow-xl z-10 border border-gray-300 dark:border-gray-600 text-gray-700 dark:text-white text-sm text-left"
       >
-        <div className="text-gray-900 dark:text-white">{userData.name}</div>
+        <div className="text-gray-900 dark:text-white">{userData.email}</div>
 
         <hr className="mt-2 pt-2 sm:mx-auto border-gray-300 dark:border-gray-600" />
 
         <ul aria-labelledby="dropdownUserAvatarButton">
-          {userprofileitems.map((userprofileitem, index) => (
-            <li key={index} className="text-md font-medium">
+          {userProfileDropdownItems.map((userprofileitem, index) => (
+            <li key={index} className="text-md font-medium me-1">
               {userprofileitem.label === "Sign out" ? (
-                <div>
-                  <hr className="mt-2 pt-2 sm:mx-auto border-gray-300 dark:border-gray-600" />
-                  <button
-                    className="w-full text-left hover:text-sky-500"
-                    onClick={signOutUser}
-                  >
-                    {userprofileitem.label}
-                  </button>
-                </div>
+                <button
+                  className="block hover:text-sky-500"
+                  onClick={handleSignOutClick}
+                >
+                  {userprofileitem.label}
+                </button>
               ) : (
                 <Link
                   to={userprofileitem.href}
+                  onClick={handleOnClick}
                   className="block hover:text-sky-500"
                 >
                   {userprofileitem.label}
