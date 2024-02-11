2d4cca65ae83d302ac61ca476dddd978a557180f
nirajgiriXD <nirajgiri9860@googlemail.com>
2024-02-12 00:38:41 +0545

    remove: semicolon

--- src/components/StockDetail/index.tsx
+++ src/components/StockDetail/index.tsx
@@ -8,43 +8,43 @@ const StockDetail = ({ stockData }: StockDataProp) => {
       <div className="flex flex-wrap justify-center gap-x-10 gap-y-2 text-xs md:text-sm font-medium">
         <div className="w-full">
           <span className="inline-block min-w-20">Symbol</span>
-          <span className="">: {stockData.symbol}</span>
+          <span className="">{stockData.symbol}</span>
         </div>
         <div className="w-full">
           <span className="inline-block min-w-20">Name</span>
-          <span className="">: {stockData.name}</span>
+          <span className="">{stockData.name}</span>
         </div>
         <div className="w-full">
           <span className="inline-block min-w-20">Sector</span>
-          <span className="">: {stockData.sector}</span>
+          <span className="">{stockData.sector}</span>
         </div>
         <div className="w-full">
           <span className="inline-block min-w-20">Open</span>
-          <span className="">: {stockData.open}</span>
+          <span className="">{stockData.open}</span>
         </div>
         <div className="w-full">
           <span className="inline-block min-w-20">High</span>
-          <span className="">: {stockData.high}</span>
+          <span className="">{stockData.high}</span>
         </div>
         <div className="w-full">
           <span className="inline-block min-w-20">Low</span>
-          <span className="">: {stockData.low}</span>
+          <span className="">{stockData.low}</span>
         </div>
         <div className="w-full">
           <span className="inline-block min-w-20">Close</span>
-          <span className="">: {stockData.close}</span>
+          <span className="">{stockData.close}</span>
         </div>
         <div className="w-full">
           <span className="inline-block min-w-20">% Change</span>
-          <span className="">: {stockData.percentage_change}</span>
+          <span className="">{stockData.percentage_change}</span>
         </div>
         <div className="w-full">
           <span className="inline-block min-w-20">Volume</span>
-          <span className="">: {stockData.volume}</span>
+          <span className="">{stockData.volume}</span>
         </div>
         <div className="w-full">
           <span className="inline-block min-w-20">Data as of</span>
-          <span className="">: {stockData.date}</span>
+          <span className="">{stockData.date}</span>
         </div>
       </div>
     </div>
