ad751030aa91c5ebec3919f8a40af5b22fd66f9b
nirajgiriXD <nirajgiri9860@googlemail.com>
2024-02-12 15:09:15 +0545

    fix: UI issue

--- src/components/Navbar/UserProfile.tsx
+++ src/components/Navbar/UserProfile.tsx
@@ -19,7 +19,7 @@ const UserProfile = () => {
 
   return (
     <>
-      <div className="flex justify-end items-end max-w-full lg:max-w-40">
+      <div className="relative flex justify-end items-end max-w-full lg:max-w-40">
         <div
           onClick={handleOnClick}
           className="flex items-center text-sm bg-gray-50 dark:bg-gray-800 rounded-full md:me-0 focus:ring-2 focus:ring-gray-300 dark:focus:ring-gray-600 border border-gray-300 dark:border-gray-600 cursor-pointer pr-3 w-full lg:max-w-max"
