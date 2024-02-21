6af7fbd1fe47195f5b862055ca57f1bae1b98848
nirajgiriXD <nirajgiri9860@googlemail.com>
2024-02-21 12:32:07 +0545

    update: services list

--- src/pages/ServicesPage/constants.tsx
+++ src/pages/ServicesPage/constants.tsx
@@ -1,177 +1,58 @@
 const ServiceItems = [
   {
-    icon: (
-      <svg
-        fill="none"
-        stroke="currentColor"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth="2"
-        className="w-6 h-6"
-        viewBox="0 0 24 24"
-      >
-        <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
-      </svg>
-    ),
+    icon: <div className="text-3xl text-center">👓</div>,
     href: "/market-watch",
     title: "Market Watch",
-    description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
+    description: "Overview of stock prices, indices, and market trends.",
   },
   {
-    icon: (
-      <svg
-        fill="none"
-        stroke="currentColor"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth="2"
-        className="w-6 h-6"
-        viewBox="0 0 24 24"
-      >
-        <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
-      </svg>
-    ),
+    icon: <div className="text-xl text-center">✔️</div>,
     href: "/ipo-checker",
     title: "IPO Checker",
     description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
+      "verify whether or not you were allotted the share and the number of shares being allotted.",
   },
   {
-    icon: (
-      <svg
-        fill="none"
-        stroke="currentColor"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth="2"
-        className="w-6 h-6"
-        viewBox="0 0 24 24"
-      >
-        <circle cx="6" cy="6" r="3"></circle>
-        <circle cx="6" cy="18" r="3"></circle>
-        <path d="M20 4L8.12 15.88M14.47 14.48L20 20M8.12 8.12L12 12"></path>
-      </svg>
-    ),
+    icon: <div className="text-xl text-center">📰</div>,
     href: "/ipo-fpo-updates",
     title: "IPO/FPO Updates",
     description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
+      "Timely updates on IPO and FPO to aid informed investment decisions.",
   },
   {
-    icon: (
-      <svg
-        fill="none"
-        stroke="currentColor"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth="2"
-        className="w-6 h-6"
-        viewBox="0 0 24 24"
-      >
-        <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
-        <circle cx="12" cy="7" r="4"></circle>
-      </svg>
-    ),
+    icon: <div className="text-xl text-center">📊</div>,
     href: "/trading-signals",
     title: "Trading Signals",
     description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
+      "Receive actionable trading signals to assist in making strategic buy or sell decisions.",
   },
   {
-    icon: (
-      <svg
-        fill="none"
-        stroke="currentColor"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth="2"
-        className="w-6 h-6"
-        viewBox="0 0 24 24"
-      >
-        <path d="M4 15s1-1 4-1 5 2 8 2 4-1 4-1V3s-1 1-4 1-5-2-8-2-4 1-4 1zM4 22v-7"></path>
-      </svg>
-    ),
+    icon: <div className="text-xl text-center">🔎</div>,
     href: "/portfolio-tracker",
     title: "Portfolio Tracker",
     description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
+      "Effortlessly monitor and manage your investments with portfolio tracker.",
   },
   {
-    icon: (
-      <svg
-        fill="none"
-        stroke="currentColor"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth="2"
-        className="w-6 h-6"
-        viewBox="0 0 24 24"
-      >
-        <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path>
-      </svg>
-    ),
-    href: "/company-analyzer",
-    title: "Company Analyzer",
-    description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
-  },
-  {
-    icon: (
-      <svg
-        fill="none"
-        stroke="currentColor"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth="2"
-        className="w-6 h-6"
-        viewBox="0 0 24 24"
-      >
-        <path d="M21 12.79A9 9 0 1111.21 3 7 7 0 0021 12.79z"></path>
-      </svg>
-    ),
+    icon: <div className="text-xl text-center">📈</div>,
     href: "/chart",
     title: "Chart",
     description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
+      "Visualize stock performance through dynamic and interactive charts.",
   },
   {
-    icon: (
-      <svg
-        fill="none"
-        stroke="currentColor"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth="2"
-        className="w-6 h-6"
-        viewBox="0 0 24 24"
-      >
-        <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path>
-      </svg>
-    ),
+    icon: <div className="text-xl text-center">⚖</div>,
     href: "/compare-company",
     title: "Compare Company",
     description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
+      "Easily compare key financial metrics between different companies.",
   },
   {
-    icon: (
-      <svg
-        fill="none"
-        stroke="currentColor"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth="2"
-        className="w-6 h-6"
-        viewBox="0 0 24 24"
-      >
-        <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path>
-      </svg>
-    ),
+    icon: <div className="text-4xl text-center">🖩</div>,
     href: "/share-calculator",
     title: "Share Calculator",
     description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
+      "Get to know the actual figures when buying and selling shares.",
   },
 ];
 
