053adf34e82cbab27e6bb87d6a007feb13afc2bf
nirajgiriXD <nirajgiri9860@googlemail.com>
2024-02-23 22:13:13 +0545

    test: api endpoint

--- src/pages/InvestingCalendarPage/index.tsx
+++ src/pages/InvestingCalendarPage/index.tsx
@@ -1,4 +1,17 @@
+/**
+ * External dependencies.
+ */
+import { useEffect } from "react";
+
 const InvestingCalendarPage = () => {
+  useEffect(() => {
+    fetch("https://nepsealpha.com/api/smx9841/investment_calander")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  });
+
   return (
     <div className="bg-gray-200 dark:bg-gray-800 p-8 rounded-md shadow-md text-center">
       <h1 className="text-2xl font-semibold mb-4">Under Construction</h1>
