c87096940bb29205811d2e65f1d8ed722dfb5871
nirajgiriXD <nirajgiri9860@googlemail.com>
2024-02-23 21:53:22 +0545

    ref: ipo checker redirect, refactor code

--- src/pages/IpoCheckerPage/index.tsx
+++ src/pages/IpoCheckerPage/index.tsx
@@ -1,9 +1,17 @@
 const IpoCheckerPage = () => {
   return (
     <div className="bg-gray-200 dark:bg-gray-800 p-8 rounded-md shadow-md text-center">
-      <h1 className="text-2xl font-semibold mb-4">Under Construction</h1>
+      <h1 className="text-2xl font-semibold mb-4">IPO Checker</h1>
       <p className="text-gray-600 dark:text-gray-400">
-        This component is currently under construction. Check back soon!
+        Please visit{" "}
+        <a
+          href="https://iporesult.cdsc.com.np/"
+          target="_blank"
+          className="text-sky-600 dark:text-sky-400"
+        >
+          AllotmentResult
+        </a>{" "}
+        in order to check IPO Result
       </p>
     </div>
   );
